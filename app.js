e521be5c32fdb03f3333e941b305216e317d66dc
Rachel Tipton <rachelmtipton@gmail.com>
2024-12-03 17:01:16 -0700

    Day 3 coding challenge commit - setting up the routes file and learning about event loops

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 const http = require('http');
+const routes = require('./routes');
+
+console.log(routes.someText);
 
 // if request comes, please execute this function
 // createServer callback function that runs anytime a request reaches our server
-const server = http.createServer((req, res) => {
-  console.log("request info", req.url, req.method, req.headers);
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
-  res.end();
-})
-
+const server = http.createServer(routes.handler);
 // listens for incoming requests on the given port
 server.listen(3000);
+
 
